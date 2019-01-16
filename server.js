dfd3297a3cf08a81ed482111877f020b2679565f
fuguxu <guangxue.fu@partner.midea.com>
2019-01-16 08:54:39 +0800

    恢复

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
         // if (ctx.url === '/test') {
         //     return "*"; // 允许来自所有域名请求
         // }
-        return 'http://localhost:8002'; // 这样就能只允许 http://localhost:8002 这个域名的请求了
+        return '*'; // 这样就能只允许 http://localhost:8002 这个域名的请求了
     },
     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
     maxAge: 5,
